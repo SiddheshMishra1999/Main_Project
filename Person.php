d2a1370f219233ea59be9340dd187838efe59a2b
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-08-06 00:22:36 -0400

    Index and Person added

---
+++ Person.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<form action="Person.php" method="post">
+Person ID: <input type="number" name="person_id"><br>
+First name: <input type="text" name="first_name"><br>
+Last name: <input type="text" name="last_name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+</body>
+</html>
+
