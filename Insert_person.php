01d07d8f485a19286ecbe9c65514d3aebf7d763d
SiddheshMishra1999 <siddhesh_mishra15@hotmail.com>
2021-08-07 12:46:13 -0400

    new changes
    still testing

--- Insert_person.php
+++ Insert_person.php
@@ -22,15 +22,64 @@ $SSN = mysqli_real_escape_string($db, $_POST['SSN']);
 $province = mysqli_real_escape_string($db, $_POST['province']);
 
 //check for missing values
-//do later. 20:00
+if(empty($person_id)) {
+  array_push($errors, "Person ID is required.");
+  echo "Person ID is required.<br />";
+  }
+  
+  if(empty($postal_code)) {
+  array_push($errors, "Postal code is required.");
+  echo "Postal code is required.<br />";
+  }
+  
+  if(empty($first_name)) {
+  array_push($errors, "First name is required.");
+  echo "First name is required.<br />";
+  }
+  
+  if(empty($last_name)) {
+  array_push($errors, "Last name is required.");
+  echo "Last name is required.<br />";
+  }
+  
+  if(empty($dob)) {
+  array_push($errors, "Date of birth is required.");
+  echo "Date of birth is required.<br />";
+  }
+  
+  if(empty($province)) {
+  array_push($errors, "Province is required.");
+  echo "Province is required.<br />";
+  }
+  if(empty($city)) {
+  array_push($errors, "City is required.");
+  echo "City is required.<br />";
+  }
+  if(empty($address)) {
+  array_push($errors, "Address is required.");
+  echo "Address is required.<br />";
+  }
+  if(empty($email)) {
+  array_push($errors, "Email is required.");
+  echo "Email is required.<br />";
+  }
+  
+  if(empty($passport_num)) {
+  $passport_num = NULL;
+  }
+  if(empty($medicare)) {
+  $medicare = NULL;
+  }
+  if(empty($SSN)) {
+  $SSN = NULL;
+  }
 
 
 //check db for existing person unique values
-$user_check_query = "SELECT * FROM Person WHERE person_id = $person_id or medicare = $medicare or SSN = $SSN or passport_num = $passport_num LIMIT 1;";
+$user_check_query = "SELECT * FROM Person WHERE person_id = '$person_id' or medicare = '$medicare' or SSN = '$SSN' or passport_num = '$passport_num' LIMIT 1";
 
 $results = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($results);
-
 if($user){
 
   if($user['person_id'] === $person_id){array_push($errors, "Person ID already exist");}
@@ -39,10 +88,13 @@ if($user){
 
 if(count($errors) == 0){
 
-  $query = "INSERT INTO Person VALUES ($person_id, $SSN, '$passport_num', '$medicare', '$first_name', '$last_name', '$dob','$telephone','$address','$email','$citizenship')";
+  $query = "INSERT INTO Person VALUES ('$person_id', '$SSN', '$passport_num', '$medicare', '$first_name', '$last_name', '$dob','$telephone','$address','$email','$citizenship','$postal_code')";
   mysqli_query($db,$query);
-  $query2 = "INSERT INTO Postal_code VALUES ('$postal_code','$city','$province');";
+  $query2 = "INSERT INTO Postal_code VALUES ('$postal_code','$city','$province')";
   mysqli_query($db,$query2);
+  // redirect to submitted page
+  header("Location: Sumbit.php", TRUE, 301);
+
 
 }
 //end registering Person
--- Person.php
+++ Person.php
@@ -6,27 +6,27 @@ include_once 'server.php'
 <html>
 <body>
 
-<form action="Insert_person.php" method="post">
+<form action="Sumbit.php" method="post">
   <div class="container">
     <h1>Add person</h1>
     <p>Please fill in this form to add a new person to the database.</p>
     <hr>
-Person ID: <input type="number" name="person_id"><br>
-First name: <input type="text" name="first_name"><br>
-Last name: <input type="text" name="last_name"><br>
-Date of Birth: <input type="date" name="dob"><br>
-Telephone Number: <input type="text" name="telephone"><br>
-Postal Code: <input type="text" name="postal_code"><br>
-Province: <input type="text" name="province"><br>
-City: <input type="text" name="city"><br>
+Person ID: <input type="number" name="person_id" required><br>
+First name: <input type="text" name="first_name" required><br>
+Last name: <input type="text" name="last_name" required><br>
+Date of Birth: <input type="date" name="dob" required><br>
+Telephone Number: <input type="text" name="telephone" required><br>
+Postal Code: <input type="text" name="postal_code" required><br>
+Province: <input type="text" name="province" required><br>
+City: <input type="text" name="city" required>><br>
 Address: <input type="text" name="address"><br>
-E-mail: <input type="text" name="email"><br>
+E-mail: <input type="text" name="email" required><br>
 Passport Number: <input type="text" name="passport_num"><br>
-Citizenship: <input type="text" name="citizenship"><br>
+Citizenship: <input type="text" name="citizenship" required><br>
 Medicare (if Canadian) <input type="text" name="medicare"><br>
 SSN (if Canadian) <input type="text" name="SSN"><br>
 
-<input type="submit">
+<input type="submit"> 
 
 </div>
 </form>
