d4fa57b8c3094d28b017254db8776fffc9541556
SiddheshMishra1999 <siddhesh_mishra15@hotmail.com>
2021-08-07 13:34:07 -0400

    css
    nav bar

--- Person.php
+++ Person.php
@@ -4,7 +4,25 @@ include_once 'server.php'
 ?>
 
 <html>
+  <head> 
+    <link rel="stylesheet" href="main.css">
+  </head>
 <body>
+<div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">Comp 353 </a>
+
+            <nav>
+                <ul class="main-nav"> 
+                    <li class="home"><a href="#">Home</a></li>
+                    <li><a href="Person.php">New Person</a></li>
+                    <li><a href="Vaccine.php">Vaccines</a></li>
+                    <li><a href="Facility.php">Facilities</a></li>
+                </ul>
+                
+            </nav>
+        </div>
+    </div>
 
 <form action="Sumbit.php" method="post">
   <div class="container">
--- Sumbit.php
+++ Sumbit.php
@@ -9,9 +9,25 @@ include_once 'server.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
+<div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">Comp 353 </a>
+
+            <nav>
+                <ul class="main-nav"> 
+                    <li class="home"><a href="#">Home</a></li>
+                    <li><a href="Person.php">New Person</a></li>
+                    <li><a href="Vaccine.php">Vaccines</a></li>
+                    <li><a href="Facility.php">Facilities</a></li>
+                </ul>
+                
+            </nav>
+        </div>
+    </div>
     <h1> Thank you! Form Submitted!  </h1> 
 </body>
 </html>
