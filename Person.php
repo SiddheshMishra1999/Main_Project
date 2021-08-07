8a0f27f4570c0b2b02ec358d4060955184a4e399
SiddheshMishra1999 <siddhesh_mishra15@hotmail.com>
2021-08-07 14:06:13 -0400

    Update Person.php

--- Person.php
+++ Person.php
@@ -25,7 +25,7 @@ include_once 'server.php'
         </div>
   </div>
 
-<form action="Sumbit.php" method="post">
+<form action="Insert_person.php" method="post">
   <div class="container">
     <h1>Add person</h1>
     <p>Please fill in this form to add a new person to the database.</p>
