c38cbefdfd4fbe05b890626d8375e935c1e9c7de
SiddheshMishra1999 <siddhesh_mishra15@hotmail.com>
2021-08-08 19:16:53 -0400

    Added nav for facility and vaccine

---
+++ Vaccine.php
@@ -0,0 +1,24 @@
+<html>
+<head> 
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">Comp 353 </a>
+
+            <nav>
+                <ul class="main-nav"> 
+                    <li><a href="Index.php">Home</a></li>
+                    <li ><a href="Person.php">New Person</a></li>
+                    <li class="vaccine">><a href="Vaccine.php">Vaccines</a></li>
+                    <li><a href="Facility.php">Facilities</a></li>
+                    <li><a href ="Workers.php">Workers</a></li>
+                </ul>
+                
+            </nav>
+        </div>
+  </div>
+
+</body>
+</html>
