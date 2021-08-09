1b2560f3060e44f795b830f085f6ae6bab85a1e8
sev8888 <40700356+sev8888@users.noreply.github.com>
2021-08-08 20:13:40 -0400

    Update Facility.php

--- Facility.php
+++ Facility.php
@@ -27,36 +27,17 @@
 
 include_once 'server.php';
 
-$query_1 = "SELECT first_name, last_name, Worker.employee_id, Worker.person_id, Works_at.facility_id, start_date, end_date
-FROM Worker, Works_at, Person, Facility
-WHERE (Worker.manages_facility_id IS NULL) 
-AND (Person.person_id = Worker.person_id) 
-AND (Works_at.facility_id = Facility.facility_id)
-AND (Worker.employee_id = Works_at.employee_id)";
 
 $results = mysqli_query($db, $query_1);
 
 echo "<table id = 'employee_table' class = 'employee_table' table border = '1'>
 
-<th> First Name </th>
-<th> Last Name </th>
-<th> Employee ID </th>
-<th> Person ID </th> 
 <th> Facility ID </th>
-<th> Start Date </th>
-<th> End Date </th>
 ";
 
 while($row = mysqli_fetch_array($results)) {
 
     echo "<tr>
-    <td> " . $row['first_name'] . "</td>
-    <td> " . $row['last_name'] . "</td>
-    <td> " . $row['employee_id'] . "</td>
-    <td> " . $row['person_id'] ."</td>
-    <td> " .$row['facility_id'] ."</td>
-    <td> " .$row['start_date'] ."</td>
-    <td> " .$row['end_date'] . "</td>
     </tr>"; 
 }
 
