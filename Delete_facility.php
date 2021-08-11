04bbe64561c47532c62059b85a0274307c19fc0d
sev8888 <40700356+sev8888@users.noreply.github.com>
2021-08-10 23:52:30 -0400

    Create Delete_facility.php

---
+++ Delete_facility.php
@@ -0,0 +1,33 @@
+<?php
+ include_once 'server.php';
+$errors = array();
+
+//Registering Facility
+
+
+$person_id = mysqli_real_escape_string($db, $_POST['facility_id']);
+
+$user_check_query = "SELECT * FROM Facility WHERE facility_id = '$facility_id' LIMIT 1";
+
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+
+if(is_null($user['facility_id'])){array_push($errors, "Facility ID doesn't exist");}
+//Prevents from deleting a person ID that does not exist in the Database.
+if($user){
+  if($user['Facility_id'] != $person_id){array_push($errors, "Facility ID doesn't exist");}
+
+}
+
+if(count($errors) == 0){
+
+  $query = "UPDATE Facility SET isactive = 'FALSE' WHERE facility_id = '$facility_id'";
+  mysqli_query($db,$query);
+  header("Location: Sumbit.php");
+}
+else{
+  header("Location: Failed.php");
+}
+//end deleting Facility
+
+?>
