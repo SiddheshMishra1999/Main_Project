f7341e9f61aaf69017ca591ef8cdf50eea3b5994
sev8888 <40700356+sev8888@users.noreply.github.com>
2021-08-11 00:27:24 -0400

    Create DeleteFacility.php

---
+++ DeleteFacility.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once 'server.php';
+?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="main.css">
+  </head>
+<body>
+
+<div class="navbar">
+        <div class="container">
+            <a class="logo" href="Index.php">Comp 353 </a>
+
+            <nav>
+                <ul class="main-nav">
+                    <li><a href="Facility.php">Person</a></li>
+                    <li class="Delete_facility"><a href="#"> > Delete a Facility</a></li>
+                </ul>
+
+            </nav>
+        </div>
+  </div>
+
+<form action="Delete_facility.php" method="post">
+  <div class="form">
+    <h1>Delete Facility</h1>
+    <p>Please enter the Facility ID that you wish to render inactive </p>
+    <hr>
+  <label id="facility_id_label" for="facility_id">Person ID: </label>
+  <input type="number" name="facility_id" id="facility_id"required><br><br>
+
+
+  </div>
+  <input type="Submit">
+
+</form>
+
+
+</body>
+</html>
+
