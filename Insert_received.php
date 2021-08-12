b6c3aebbc02ea33fe5dece635a7f4f247d2c2947
Jason Hillinger <57076835+jasonhillinger@users.noreply.github.com>
2021-08-11 23:51:48 -0400

    Updates

--- Insert_received.php
+++ Insert_received.php
@@ -22,12 +22,25 @@ $dose_num = $user1['COUNT(dose_num)'];
 
 
 //Error Check# 1: Employee, Checks if employee works at facility.
-// $user_check_query = "SELECT * FROM Works_at, Facility WHERE end_date IS NULL AND Works_at.facility_id = '$facility_id'";
-// $results = mysqli_query($db, $user_check_query);
-// $user = mysqli_fetch_assoc($results);
-// if($user){
-//   if($user['Works_at.facility_id'] != $facility_id ){array_push($errors, "Worker does not work at that facility");}
-// }
+
+$user_check_query = "SELECT * FROM Works_at, Facility WHERE Works_at.facility_id = Facility.facility_id
+AND end_date IS NOT NULL
+AND Facility.facility_id = '$facility_id'
+AND Works_at.employee_id = '$employee_id'";
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+if($user){
+  if($user['employee_id'] == $employee_id ){array_push($errors, "Worker does not work at that facility (quit)");}
+}
+
+$user_check_query = "SELECT * FROM Works_at, Facility WHERE Works_at.facility_id = Facility.facility_id
+AND Works_at.employee_id = '$employee_id'";
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+if($user){
+  if($user['facility_id'] != $facility_id ){array_push($errors, "Worker does not work at that facility (quit)");}
+}
+
 //////////////////////////////////////////////////////////////////
 
 
@@ -43,7 +56,7 @@ if($user){
 
 
 
-//Error Check# 3: Available Vaccine inventory, checks if there is more than 0 vaccines available.
+//Error Check# 3: Available Vaccine inventory, checks if there is more than 0 vaccines available and if the facility is active.
 $user_check_query = "SELECT * FROM Facility,Inventory
 WHERE Facility.facility_id = Inventory.facility_id
 AND Inventory.vaccine_id = '$vaccine_id'
@@ -52,8 +65,30 @@ $results = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($results);
 if($user){
   if($user['amount'] == 0){array_push($errors, "Not enough vaccines in inventory");}
+  if($user['isactive'] == "FALSE"){array_push($errors, "Facility is closed");}
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Error Check# 4: Age check
+$user_check_query = "SELECT *
+FROM Person,AgeGroup ,Admin, Facility , Postal_code, Received
+WHERE Person.person_id = $person_id AND
+Facility.postal_code = Postal_code.postal_code AND
+Admin.Province = Postal_code.province AND
+Admin.eligible_GroupID >= AgeGroup.GroupID AND
+Received.facility_id = $facility_id AND
+(FLOOR(DATEDIFF($date_received, Person.dob)/365.25) >= min_age) LIMIT 1 )";
+
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+if($user){
+  if($user['person_id'] == $person_id){array_push($errors, "Person is in wrong age group");}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 
 //Vaccination Process
